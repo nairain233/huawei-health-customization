bb501b4e6ffb6788120f75e8eeae1d6d3c0d9491
nairain <nairain233@gmail.com>
2026-09-06 21:13:26 +0800

    chore(release): 发布 v1.1
    - 更新 versionCode 与 versionName
    - 添加 v1.1 更新日志

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "love.nairain.huawei"
         minSdk = 28
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
