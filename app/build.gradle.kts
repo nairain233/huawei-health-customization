28a31b00a949e12b2b96ecf1812b3519a6555e5b
nairain <nairain233@gmail.com>
2026-09-09 22:49:29 +0800

    chore(release): 发布 v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "love.nairain.huawei"
         minSdk = 28
         targetSdk = 37
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
